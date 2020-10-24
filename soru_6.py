0bffeba23c8f598518159e0bc2fbb18ee2bd2384
AhmetMeteErol <72703850+AhmetMeteErol@users.noreply.github.com>
2020-10-24 16:27:58 +0300

    Update soru_6.py

--- soru_6.py
+++ soru_6.py
@@ -5,5 +5,5 @@ count = 0
 for i in range(1000,10000):
     if (int(str(i)[0])) > (int(str(i)[-1])):        # First we convert 2 str
         print(i)                                    # 2 reach first digit
-        count += 1                                            # then, reconvert that number as int
+        count += 1                                  # then, reconvert that number as int
 print(f"There're {count} numbers.")    
